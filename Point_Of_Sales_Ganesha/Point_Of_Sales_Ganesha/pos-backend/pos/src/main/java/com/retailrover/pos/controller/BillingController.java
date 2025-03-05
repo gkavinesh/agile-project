76ba012691130a299936363a46bbccf4ed82b124
gkavinesh <gkavinesh186@gmail.com>
2025-03-05 21:26:33 +0530

    Commit

--- Point_Of_Sales_Ganesha/Point_Of_Sales_Ganesha/pos-backend/pos/src/main/java/com/retailrover/pos/controller/BillingController.java
+++ Point_Of_Sales_Ganesha/Point_Of_Sales_Ganesha/pos-backend/pos/src/main/java/com/retailrover/pos/controller/BillingController.java
@@ -3,9 +3,7 @@ package com.retailrover.pos.controller;
 import com.retailrover.pos.entity.Billing;
 import com.retailrover.pos.service.BillingService;
 import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,44 +11,42 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor
 @CrossOrigin("*")
 public class BillingController {
 
     private final BillingService billingService;
 
+    public BillingController(BillingService billingService) {
+        this.billingService = billingService; // Initialization via constructor
+    }
+
     @PostMapping("/billing")
-    public Billing postbilling(@RequestBody Billing billing) {
+    public Billing postBilling(@RequestBody Billing billing) {
         return billingService.post(billing);
     }
 
     @GetMapping("/billing")
-    public List<Billing> getAllBilling(){
+    public List<Billing> getAllBilling() {
         return billingService.getAllBillings();
     }
 
     @DeleteMapping("/billing/{id}")
-    public ResponseEntity<?> deleteBilling(@PathVariable Long id){
-        try{
+    public ResponseEntity<String> deleteBilling(@PathVariable Long id) {
+        try {
             billingService.deleteBilling(id);
-            return new ResponseEntity<>( "User with ID" + id + " deleted successfully", HttpStatus.OK);
-
-        }catch(EntityNotFoundException e){
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("User with ID " + id + " deleted successfully", HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/billing/{id}")
-    public ResponseEntity<?> getBillingByID(@PathVariable Long id){
+    public ResponseEntity<Billing> getBillingByID(@PathVariable Long id) {
         Billing billing = billingService.getBillingByID(id);
-        if(billing == null) return ResponseEntity.notFound().build();
+        if (billing == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(billing);
     }
-
-
-
-
-
-
-
 }
+
